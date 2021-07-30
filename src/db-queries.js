43f7b24417663f8eef3c1380047d2fbbcddfd8dd
Johnny <jguo45@gmail.com>
2021-07-30 11:33:12 -0400

    bot accepts any text that doesn't start with '!' as a message. messages are stored in a sql database. messages are viewed by writing them to a text file and returning it.

---
+++ src/db-queries.js
@@ -0,0 +1,39 @@
+module.exports = {
+  createTable(db) {
+    const query = db.prepare(
+      `CREATE TABLE IF NOT EXISTS mailbox (message TEXT , author TEXT);`
+    )
+    query.run();
+  },
+
+  addMessage(db, message, author) {
+    const query = db.prepare(
+      `INSERT INTO mailbox(message, author) VALUES (?,?);`
+      );
+    query.run(message, author);
+  },
+
+  openMailbox(db) {
+    const query = db.prepare(
+      'SELECT * FROM mailbox;'
+    );
+
+    return query;
+  },
+
+  getNumMessages(db) {
+    const query = db.prepare(
+      `SELECT COUNT(message) FROM mailbox;`
+    );
+    const result = query.get();
+    return result[Object.keys(result)[0]];
+  },
+
+  clearTable(db) {
+    const query = db.prepare(
+      `DROP TABLE mailbox;`
+    );
+    query.run();
+    this.createTable(db);
+  }
+};
--- src/index.js
+++ src/index.js
@@ -1,64 +1,122 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
-require('dotenv').config();
-const PREFIX = '!';
-// const Members = client.guilds.cache.get("GuildID").members.map()
+const Database = require("better-sqlite3");
+const path = require("path");
+const fs = require('fs');
+require("dotenv").config();
 
+const queries = require("../src/db-queries");
+const db = new Database(path.resolve("data/mailbox.db"));
+const mailboxDir = 'data/mailbox.txt';
+
+const PREFIX = "!";
+const adminID = ['143032936952758272', '528030849439105044'];
 
 // Login
-client.once('ready', () => {
+client.once("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
+  client.user.setActivity("for messages | !help", { type: "WATCHING" });
+  queries.createTable(db);
 });
 
-client.once('reconnecting', () => {
-  console.log('Reconnecting!');
+client.once("reconnecting", () => {
+  console.log("Reconnecting!");
 });
 
-client.once('disconnect', () => {
-  console.log('Disconnected!');
+client.once("disconnect", () => {
+  console.log("Disconnected!");
 });
 
-client.on('message', (msg) => {
-  if (!msg.content.startsWith(PREFIX) || msg.author.bot) return;
+client.on("message", (msg) => {
+  if (!msg.content.startsWith(PREFIX) || !(msg.channel instanceof Discord.DMChannel) || !adminID.includes(msg.author.id) || msg.author.bot) return;
+
+  const filter = (m) =>
+    m.author.id === msg.author.id && m.channel instanceof Discord.DMChannel;
+  const collector = msg.channel.createMessageCollector(filter, {
+    max: 1,
+  });
 
   const args = msg.content.slice(PREFIX.length).trim().split(/ +/);
   const commandName = args.shift().toLowerCase();
 
-  if (commandName == 'hewwo') {
-    return msg.channel.send('Hewwo!!');
+  // if (commandName === "message") {
+  //   msg.channel.send("Send your message");
+
+  //   collector.on("collect", (m) => {
+  //     console.log(`Collected ${m.content}`);
+  //     queries.addMessage(db, m.content, msg.author.username);
+  //   });
+
+  //   collector.on("end", (collected) => {
+  //     // console.log(`Collected ${collected.size} items`);
+  //     msg.channel.send("Thank you for your message");
+
+  //     collected.each((value) => {
+  //       console.log(value.content);
+  //     });
+  //   });
+  // }
+
+  if (commandName === "view") {
+    const mailbox = queries.openMailbox(db);
+    
+    fs.writeFileSync(mailboxDir, '');
+    let counter = 0;
+    let allMessages = "";
+    for (const mail of mailbox.iterate()) {
+      fs.appendFileSync(mailboxDir, `${counter++}. ${mail.author} : ${mail.message}\n`, err => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+      });
+      // msg.channel.send(`${counter++}. ${mail.author} : ${mail.message}\n`);
+      // allMessages += `${counter++}. ${mail.author} : ${mail.message}\n`;
+    }
+    // msg.channel.send(allMessages);
+    msg.channel.send({ files: ["./data/mailbox.txt"] });
   }
 
-  if (commandName == 'bee') {
-    return msg.channel.send('my creator is johnny and he’s the smartest person in the world');
+  else if (commandName === "total") {
+    console.log(queries.getNumMessages(db));
   }
 
-  if (commandName == 'iris') {
-    return msg.channel.send('the most beautiful person to grace my pupils. i adore and love her so much');
+  else if (commandName === 'clear') {
+    queries.clearTable(db);
+    console.log('Cleared Table');
   }
 
-  if (commandName == 'peepee') {
-    return msg.channel.send('poopoo');
-  }
+  // else {
+  //   console.log(`Collected ${msg.content}`);
+  //   queries.addMessage(db, msg.content, msg.author.username);
+  //   msg.channel.send("Thank you for your message");
+  // }
+});
 
-  if (commandName == 'shop') {
-    return msg.channel.send({
-      files: ['./images/shopjep.jpg'],
-      'Welcome to my shop!\n Look upon my wares.',
-    });
-  }
+client.on('message', (msg) => {
+  if (msg.author.bot || msg.content.startsWith(PREFIX)) return;
 
-  if (commandName == 'baybee') {
-    return msg.channel.send('is me!!');
-  }
-
+  console.log(`Collected ${msg.content}`);
+  queries.addMessage(db, msg.content, msg.author.username);
+  msg.channel.send("Thank you for your message");
 });
 
 // client.on('message', (msg) => {
-//   if (msg.author.id === JOHNNY) {
-//     if (msg.content.search('tomato') >= 0) {
-//       msg.channel.send('potato');
-//     }
+//   if (msg.author.bot) return;
+//   else if (msg.channel instanceof Discord.DMChannel) {
+//     console.log(msg.content);
+//     msg.author.send("You're DMing me now!")
+//       .then(msg => console.log(`Sent message: ${msg.content}`))
+//       .catch(console.error);
+//     return;
 //   }
-// })
+// });
+
+// client.on("message", (msg) => {
+//   if (!msg.content.startsWith(PREFIX) || msg.author.bot) return;
+
+//   const args = msg.content.slice(PREFIX.length).trim().split(/ +/);
+//   const commandName = args.shift().toLowerCase();
+// });
 
 client.login(process.env.CLIENT_ID);
