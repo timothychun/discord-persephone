6c2b6bf8fde58227b021f2098491d713471a42dc
Johnny <jguo45@gmail.com>
2021-07-30 19:36:28 -0400

    added help command

--- src/index.js
+++ src/index.js
@@ -2,20 +2,21 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const Database = require("better-sqlite3");
 const path = require("path");
-const fs = require('fs');
+const fs = require("fs");
 require("dotenv").config();
 
 const queries = require("../src/db-queries");
 const db = new Database(path.resolve("data/mailbox.db"));
-const mailboxDir = './data/mailbox.txt';
+const mailboxDir = "./data/mailbox.txt";
 
 const PREFIX = "!";
-const adminID = process.env.ADMINID.split(' ');
+const adminID = process.env.ADMINID.split(" ");
+const cooldowns = new Discord.Collection();
 
 // Login
 client.once("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
-  client.user.setActivity("for messages!", { type: "WATCHING" });
+  client.user.setActivity("for messages! | !help", { type: "WATCHING" });
   queries.createTable(db);
 });
 
@@ -29,10 +30,13 @@ client.once("disconnect", () => {
 
 // restricted bot commands
 client.on("message", (msg) => {
-  if (!msg.content.startsWith(PREFIX) || 
-      !(msg.channel instanceof Discord.DMChannel) || 
-      !adminID.includes(msg.author.id) || 
-      msg.author.bot) return;
+  if (
+    !msg.content.startsWith(PREFIX) ||
+    !(msg.channel instanceof Discord.DMChannel) ||
+    !adminID.includes(msg.author.id) ||
+    msg.author.bot
+  )
+    return;
 
   const args = msg.content.slice(PREFIX.length).trim().split(/ +/);
   const commandName = args.shift().toLowerCase();
@@ -41,38 +45,56 @@ client.on("message", (msg) => {
   if (commandName === "open") {
     msg.channel.send({ files: [mailboxDir] });
   }
-  
+
   // clears the mailbox
-  else if (commandName === 'clear') {
-    fs.writeFileSync(mailboxDir, '');
+  else if (commandName === "clear") {
+    fs.writeFileSync(mailboxDir, "");
 
     queries.createTable(db);
 
-    console.log('Cleared Table');
-    msg.channel.send('Cleared Table');
+    console.log("Cleared Table");
+    msg.channel.send("Cleared Table");
+  }
+});
+
+// unrestricted bot commands
+client.on('message', (msg) => {
+  if (!msg.content.startsWith(PREFIX) || msg.author.bot) return;
+
+  const args = msg.content.slice(PREFIX.length).trim().split(/ +/);
+  const commandName = args.shift().toLowerCase();
+
+  if (commandName === 'help') {
+    msg.channel.send(`Hello there ${msg.author.username}!\nThis bot is very bare bones at the moment. Currently it can only take messages you send it and stores them! Iris wanted this to be a suggestion box of sorts for the server but feel free to send it whatever you want! If you have suggestions for the bot as well, send them here too!\n(As of now any message starting with '!' is reserved for commands only but this may be subject to change)`);
   }
 });
 
 // stores any dm to the bot that doesn't start with '!' as a message
-client.on('message', (msg) => {
-  if (msg.author.bot || 
-      msg.content.startsWith(PREFIX) || 
-      !(msg.channel instanceof Discord.DMChannel) ) return;
+client.on("message", (msg) => {
+  if (
+    msg.author.bot ||
+    msg.content.startsWith(PREFIX) ||
+    !(msg.channel instanceof Discord.DMChannel)
+  )
+    return;
 
   console.log(`Collected ${msg.content}`);
-  
-  // writes the message into a text file
-  fs.appendFileSync(mailboxDir, `${msg.author.username} : ${msg.content}\n`, err => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-  });
 
-  if (msg.content === 'pee') {
-    msg.channel.send('bro wtf why?');
-  }
-  else {
+  // writes the message into a text file
+  fs.appendFileSync(
+    mailboxDir,
+    `${msg.author.username} : ${msg.content}\n`,
+    (err) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+    }
+  );
+
+  if (msg.content === "pee") {
+    msg.channel.send("bro wtf why?");
+  } else {
     msg.channel.send("Thank you for your message!");
   }
 });
